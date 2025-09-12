1bcd01a015f97fc4e50df3c7c361327b713f5411
Poovarasan0909 <poovarasanm0909@gmail.com>
2025-09-12 09:57:38 +0530

    new imp

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 import { GoogleGenAI  } from "@google/genai";
-
+const corsHeaders = {
+    'Access-Control-Allow-Origin': 'http://localhost:4000',
+    'Access-Control-Allow-Methods': 'POST, OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type',
+};
 export default {
 
     async fetch(request, env) {
         const url = new URL(request.url);
+        if (request.method === 'OPTIONS') {
+            return new Response(null, { headers: corsHeaders });
+        }
 
-        if (url.pathname === "/generate") {
+        if (url.pathname === "/generate" && request.method === "POST") {
             try {
+                const requestBody = await request.json();
+                console.log("Generating content using Google GenAI...", requestBody);
                 const ai = new GoogleGenAI({ apiKey: env.GOOGLE_API_KEY });
                 const response = await ai.models.generateContent({
                     model: "gemma-3-27b-it",
-                    contents: "Roses are red, violets are blue...",
+                    contents: requestBody.userPrompt,
                 });
 
                 return new Response(
                     JSON.stringify({ result: response.text }),
-                    { headers: { "Content-Type": "application/json" } }
+                    { headers: {...corsHeaders, "Content-Type": "application/json" } }
                 );
             } catch (error) {
                 return new Response(
@@ -26,7 +35,7 @@ export default {
         }
         
         if(url.pathname === "/") {
-            return new Response("Welcome to the GenAI API", { status: 200 });
+            return new Response("Welcome to the GenAI API", { status: 200, headers: {...corsHeaders, "Content-Type": "text/plain" } });
         }
         return new Response("Not Found", { status: 404 });
     }
